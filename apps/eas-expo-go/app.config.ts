80459a5ca4f94b6d003c2f19d5f08da93b84da7e
nishan (o^▽^o) <nishanbende@gmail.com>
2025-08-05 13:43:04 +0530

    wip

--- apps/eas-expo-go/app.config.ts
+++ apps/eas-expo-go/app.config.ts
@@ -10,7 +10,7 @@ const mapBuildProfileToConfig: Record<string, ExpoConfig> = {
     name: 'Expo Go (versioned) + add sdk',
     extra: {
       eas: {
-        projectId: '8550a402-d28f-4437-af87-2fb77b576c3f',
+        projectId: '3d7abddb-e00a-4c01-a613-db3de44dd8dd',
       },
     },
   },
@@ -20,7 +20,7 @@ const mapBuildProfileToConfig: Record<string, ExpoConfig> = {
     name: 'Expo Go (versioned)',
     extra: {
       eas: {
-        projectId: '97ab66f4-49e2-4ec7-85cc-922c56a68bae',
+        projectId: '3d7abddb-e00a-4c01-a613-db3de44dd8dd',
       },
     },
   },
@@ -30,7 +30,7 @@ const mapBuildProfileToConfig: Record<string, ExpoConfig> = {
     name: 'Expo Go (unversioned)',
     extra: {
       eas: {
-        projectId: '09066dbe-ef65-460e-9201-b7aa931abbf4',
+        projectId: '3d7abddb-e00a-4c01-a613-db3de44dd8dd',
       },
     },
   },
@@ -40,7 +40,7 @@ const mapBuildProfileToConfig: Record<string, ExpoConfig> = {
     name: 'Expo Go',
     extra: {
       eas: {
-        projectId: '79a64298-2d61-42ae-9cc9-b2a358d6869e',
+        projectId: '3d7abddb-e00a-4c01-a613-db3de44dd8dd',
       },
     },
   },
